6d00e4f1a6eb9eb9b04afe905b46359cfb4c8431
wangning <kenibote@sina.com>
2018-01-11 22:20:49 +0800

    增加了FileServerV2版本
    TEST了之前的RemoteBalanceServer类
    TEST了之前的findserver函数
    增加了FileServerV2版本
    ---------------------------------------------
    目前LocalServer服务正常，需要再次检查socket逻辑。

---
+++ src/base/FileServerV2.java
@@ -0,0 +1,105 @@
+package base;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class FileServerV2 implements Runnable {
+	/**
+	 * 文件服务子线程，在创建该线程时候需要初始化端口号; 该子函数，首先由客户端提供一个文件名，之后向用户传递指定文件。
+	 * v2与v1的不同在于，v2不需要打开文件，读取文件，而是直接从内存中传递一个指定的内容！！！ size可以指定传递文件的大小。
+	 */
+	private static Logger logger = LogManager.getLogger(FileServer.class.getName());
+
+	private int port = 8001;
+	private int size = 100;
+	// 用于异步关闭线程
+	private ServerSocket ss = null;
+
+	// 构造器
+	public FileServerV2(int serverport, int s) {
+		this.port = serverport;
+		this.size = s;
+	}
+
+	// 关闭线程
+	// 关闭后会自动转入异常处理模块，之后会自动标记线程为关闭状态
+	public void stopServer() {
+		try {
+			ss.close();
+		} catch (IOException e) {
+			logger.error("异步关闭FileServer发生错误！！！");
+		}
+	}
+
+	@Override
+	public void run() {
+		Socket s = null;
+		// ----------------------------
+		DataInputStream dis = null;
+		DataOutputStream ps = null;
+
+		try {
+			// ss被提前，用于异步关闭线程
+			ss = new ServerSocket(port);
+			// 如果启动成功,设置该为可用
+			FileServerControl.setFileServerStatus(port, FileServerStatus.AVAILABLE);
+
+			while (true) {
+				// 建立socket链接
+				s = ss.accept();
+				logger.info("Port:" + port + ", Build Socket Link...");
+
+				// 取得需要下载的文件名
+				dis = new DataInputStream(new BufferedInputStream(s.getInputStream()));
+				ps = new DataOutputStream(s.getOutputStream());
+
+				String filePath = dis.readUTF();
+				logger.info("Port:" + port + ", File:" + filePath);
+
+				// 传送文件
+				ps.writeUTF(filePath);
+				ps.flush();
+				ps.writeLong((long) 8192 * size);
+				ps.flush();
+
+				int bufferSize = 8192;
+				byte[] buf = new byte[bufferSize];
+
+				for (int i = 0; i < bufferSize; i++)
+					buf[i] = (byte) i;
+
+				for (int i = 1; i <= size; i++)
+					ps.write(buf, 0, bufferSize);
+
+				ps.flush();
+
+				// 关闭文件流和socket链接
+				// fis.close();
+				s.close();
+				logger.info("Port:" + port + ", File Tran Completed.");
+				// 重新登记该端口可用
+				FileServerControl.setFileServerStatus(port, FileServerStatus.AVAILABLE);
+			}
+
+		} catch (IOException e) {
+			logger.error("Port:" + port + " Closed!!!");
+			// TODO 发生任何错误之后，使得该端口不可用，需要在FileServerControl里面进行相关处理
+			// 目前设置为不可用，FileServerControl会尝试重启
+			FileServerControl.setFileServerStatus(port, FileServerStatus.OFF);
+		}
+
+	}// end run
+
+	// 测试代码
+	public static void main(String[] args) {
+		new Thread(new FileServer(8821)).start();
+	}
+
+}
