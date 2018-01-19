55b3a5e6daf3584f1eb7783ad6ab6ba514dbca97
wangning <kenibote@sina.com>
2018-01-19 22:35:32 +0800

    Monitor可视化框架

--- src/monitor/MonitorSetting.java
+++ src/monitor/MonitorSetting.java
@@ -1,6 +1,7 @@
 package monitor;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -12,21 +13,21 @@ public class MonitorSetting {
 
 	public static HashMap<Integer, String> local_server_info = new HashMap<>();
 	public static HashMap<Integer, String> user_server_info = new HashMap<>();
-	public static String original_server = "173.0.0.1";
+	public static String original_server = "10.10.12.136";
 	public static String monitor_ip = "10.10.12.136";
 
-	{
-		local_server_info.put(1, "10.10.12.92");
-		local_server_info.put(2, "10.10.12.100");
-		local_server_info.put(3, "10.10.12.29");
+	static {
+		local_server_info.put(1, "10.10.12.110");
+		local_server_info.put(2, "10.10.12.33");
+		local_server_info.put(3, "10.10.12.59");
 
-		user_server_info.put(1, "192.168.1.92");
-		user_server_info.put(2, "192.168.1.100");
-		user_server_info.put(3, "192.168.1.29");
+		user_server_info.put(1, "192.168.1.110");
+		user_server_info.put(2, "192.168.1.33");
+		user_server_info.put(3, "192.168.1.59");
 	}
 
 	public static String setLocalServer(int id) throws Exception {
-		Socket socket = new Socket(local_server_info.get(id), 8091);
+		Socket socket = new Socket(local_server_info.get(id), 8090);
 
 		BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		PrintWriter write = new PrintWriter(socket.getOutputStream());
@@ -158,12 +159,15 @@ public class MonitorSetting {
 		HashMap<String, String> result = new HashMap<>();
 		result.put("KEY", "LocalServer");
 		result.put("TYPE", "TASK_014");
-		result.put("LocalServer1",
-				"C1-C2-C3-C4-C5-C6-C7-C8-C9-C10-C11-C12-C13-C14-C15-C16-C17-C18-C19-C20-C23-C24-C27-C32-C34-C37-C39-C44-C45-C50-C52-C55-C57-C62-C65-C66-C70-C73-C75-C80-C83-C84-C87-C92-C95-C96-C100-C103-C106-C109");
-		result.put("LocalServer2",
-				"C1-C2-C3-C4-C5-C6-C7-C8-C9-C10-C11-C12-C13-C14-C15-C16-C17-C18-C19-C20-C22-C25-C28-C31-C33-C38-C40-C43-C46-C49-C51-C56-C59-C60-C63-C68-C71-C72-C76-C79-C81-C86-C89-C90-C93-C98-C101-C102-C105-C110");
-		result.put("LocalServer3",
-				"C1-C2-C3-C4-C5-C6-C7-C8-C9-C10-C11-C12-C13-C14-C15-C16-C17-C18-C19-C20-C21-C26-C29-C30-C35-C36-C41-C42-C47-C48-C53-C54-C58-C61-C64-C67-C69-C74-C77-C78-C82-C85-C88-C91-C94-C97-C99-C104-C107-C108");
+
+		BufferedReader fin = new BufferedReader(new FileReader("D:\\Content\\Map.txt"));
+		String readin = null;
+		while ((readin = fin.readLine()) != null) {
+			String[] sp = readin.split(":");
+
+			result.put(sp[0], sp[1]);
+		}
+		fin.close();
 
 		JSONObject json = JSONObject.fromObject(result);
 		write.println(json.toString());
---
+++ src/monitor/MonitorWindow.java
@@ -0,0 +1,369 @@
+package monitor;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import net.sf.json.JSONObject;
+
+@SuppressWarnings("serial")
+public class MonitorWindow extends JFrame {
+	public static JPanel panel_server, panel_original, panel_user, panel_info;
+	public static JButton b_server_set, b_server_hello, b_server_init, b_server_updatamap, b_server_start,
+			b_server_stop, b_original_set, b_original_init, b_original_hello, b_original_start, b_original_stop,
+			b_user_set, b_user_load, b_user_start, b_user_check, b_collectdata;
+	public static JLabel task1, task2, task3;
+	public static JTextField[] progress = new JTextField[4];
+
+	public MonitorWindow() {
+		panel_server = new JPanel();
+		panel_original = new JPanel();
+		panel_user = new JPanel();
+		panel_info = new JPanel();
+
+		// 初始化按钮
+		b_server_set = new JButton("设置本地服务器");
+		b_server_hello = new JButton("HELLO");
+		b_server_updatamap = new JButton("更新Map");
+		b_server_init = new JButton("初始化");
+		b_server_start = new JButton("启动");
+		b_server_stop = new JButton("STOP");
+
+		b_original_set = new JButton("设置原服务器");
+		b_original_hello = new JButton("HELLO");
+		b_original_init = new JButton("初始化");
+		b_original_start = new JButton("启动");
+		b_original_stop = new JButton("STOP");
+
+		b_user_set = new JButton("设置用户仿真");
+		b_user_load = new JButton("读取TASK");
+		b_user_start = new JButton("开始仿真");
+		b_user_check = new JButton("Check");
+		b_collectdata = new JButton("回收实验数据");
+
+		task1 = new JLabel("User1:");
+		task2 = new JLabel("User2:");
+		task3 = new JLabel("User3:");
+		progress[1] = new JTextField();
+		progress[2] = new JTextField();
+		progress[3] = new JTextField();
+		progress[1].setPreferredSize(new Dimension(60, 25));
+		progress[2].setPreferredSize(new Dimension(60, 25));
+		progress[3].setPreferredSize(new Dimension(60, 25));
+		progress[1].setText(" 0.0% ");
+		progress[2].setText(" 0.0% ");
+		progress[3].setText(" 0.0% ");
+
+		// 设置布局
+		this.setLayout(new GridLayout(4, 1));
+
+		// 添加按钮
+		panel_server.add(b_server_set);
+		panel_server.add(b_server_hello);
+		panel_server.add(b_server_updatamap);
+		panel_server.add(b_server_init);
+		panel_server.add(b_server_start);
+		panel_server.add(b_server_stop);
+
+		panel_original.add(b_original_set);
+		panel_original.add(b_original_hello);
+		panel_original.add(b_original_init);
+		panel_original.add(b_original_start);
+		panel_original.add(b_original_stop);
+
+		panel_user.add(b_user_set);
+		panel_user.add(b_user_load);
+		panel_user.add(b_user_start);
+		panel_user.add(b_user_check);
+		panel_user.add(b_collectdata);
+
+		panel_info.add(task1);
+		panel_info.add(progress[1]);
+		panel_info.add(task2);
+		panel_info.add(progress[2]);
+		panel_info.add(task3);
+		panel_info.add(progress[3]);
+
+		// 加入到JFrame
+		this.add(panel_server);
+		this.add(panel_original);
+		this.add(panel_user);
+		this.add(panel_info);
+
+		// 添加动作
+		addAction();
+
+		// 设置窗体
+		this.setTitle("Super Monitor -- WangNing");// 窗体标签
+		this.setSize(600, 160);// 窗体大小
+		this.setLocationRelativeTo(null);// 在屏幕中间显示(居中显示)
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// 退出关闭JFrame
+		this.setVisible(true);// 显示窗体
+
+		// 锁定窗体
+		this.setResizable(false);
+	}
+
+	public static void addAction() {
+		b_server_updatamap.setBackground(Color.yellow);
+		b_server_start.setBackground(Color.green);
+		b_server_stop.setBackground(Color.red);
+
+		b_original_start.setBackground(Color.green);
+		b_original_stop.setBackground(Color.red);
+
+		b_user_load.setBackground(Color.orange);
+		b_user_start.setBackground(Color.green);
+		b_collectdata.setBackground(Color.magenta);
+
+		b_server_set.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				for (int i = 1; i <= 3; i++) {
+					String back = null;
+					try {
+						back = MonitorSetting.setLocalServer(i);
+					} catch (Exception e1) {
+						System.out.println("Local Server Set Fail !!!");
+					}
+					System.out.println(back);
+				}
+			}
+		});
+
+		b_server_hello.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				for (int i = 1; i <= 3; i++) {
+					String back = null;
+					try {
+						back = MonitorSetting.Usually(MonitorSetting.local_server_info.get(i), 8090, "LocalServer",
+								"TASK_001");
+					} catch (Exception e1) {
+						System.out.println("Local Server HELLO Fail !!!");
+					}
+					System.out.println(back);
+				}
+			}
+		});
+
+		b_server_updatamap.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				for (int i = 1; i <= 3; i++) {
+					String back = null;
+					try {
+						back = MonitorSetting.SetLocalServerMap(i);
+					} catch (Exception e1) {
+						System.out.println("Local Server UpdataMap Fail !!!");
+					}
+					System.out.println(back);
+				}
+			}
+		});
+
+		b_server_init.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				for (int i = 1; i <= 3; i++) {
+					String back = null;
+					try {
+						back = MonitorSetting.Usually(MonitorSetting.local_server_info.get(i), 8090, "LocalServer",
+								"TASK_004");
+					} catch (Exception e1) {
+						System.out.println("Local Server Init Fail !!!");
+					}
+					System.out.println(back);
+				}
+			}
+		});
+
+		b_server_start.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				for (int i = 1; i <= 3; i++) {
+					String back = null;
+					try {
+						back = MonitorSetting.Usually(MonitorSetting.local_server_info.get(i), 8090, "LocalServer",
+								"TASK_005");
+					} catch (Exception e1) {
+						System.out.println("Local Server Start Fail !!!");
+					}
+					System.out.println(back);
+				}
+			}
+		});
+
+		b_server_stop.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				for (int i = 1; i <= 3; i++) {
+					String back = null;
+					try {
+						back = MonitorSetting.Usually(MonitorSetting.local_server_info.get(i), 8090, "LocalServer",
+								"TASK_006");
+					} catch (Exception e1) {
+						System.out.println("Local Server Stop Fail !!!");
+					}
+					System.out.println(back);
+				}
+			}
+		});
+
+		// ----------------------------------------------------------------
+		b_original_set.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String back = null;
+				try {
+					back = MonitorSetting.setOriginalServer();
+				} catch (Exception e1) {
+					System.out.println("OriginalServer Set Fail !!!");
+				}
+				System.out.println(back);
+			}
+		});
+
+		b_original_hello.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String back = null;
+				try {
+					back = MonitorSetting.Usually(MonitorSetting.original_server, 8090, "OriginalServer", "TASK_001");
+				} catch (Exception e1) {
+					System.out.println("OriginalServer Hello Fail !!!");
+				}
+				System.out.println(back);
+			}
+		});
+
+		b_original_init.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String back = null;
+				try {
+					back = MonitorSetting.Usually(MonitorSetting.original_server, 8090, "OriginalServer", "TASK_008");
+				} catch (Exception e1) {
+					System.out.println("OriginalServer Init Fail !!!");
+				}
+				System.out.println(back);
+			}
+		});
+
+		b_original_start.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String back = null;
+				try {
+					back = MonitorSetting.Usually(MonitorSetting.original_server, 8090, "OriginalServer", "TASK_009");
+				} catch (Exception e1) {
+					System.out.println("OriginalServer Start Fail !!!");
+				}
+				System.out.println(back);
+			}
+		});
+
+		b_original_stop.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String back = null;
+				try {
+					back = MonitorSetting.Usually(MonitorSetting.original_server, 8090, "OriginalServer", "TASK_010");
+				} catch (Exception e1) {
+					System.out.println("OriginalServer Stop Fail !!!");
+				}
+				System.out.println(back);
+			}
+		});
+
+		// ----------------------------------------------------------------
+		b_user_set.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				for (int i = 1; i <= 3; i++) {
+					String back = null;
+					try {
+						back = MonitorSetting.setUser(i);
+					} catch (Exception e1) {
+						System.out.println("User Set Fail !!!");
+					}
+					System.out.println(back);
+				}
+			}
+		});
+
+		b_user_load.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				for (int i = 1; i <= 3; i++) {
+					String back = null;
+					try {
+						back = MonitorSetting.UserLoadTask(i);
+					} catch (Exception e1) {
+						System.out.println("User Load Fail !!!");
+					}
+					System.out.println(back);
+				}
+			}
+		});
+
+		b_user_start.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				for (int i = 1; i <= 3; i++) {
+					String back = null;
+					try {
+						back = MonitorSetting.Usually(MonitorSetting.local_server_info.get(i), 8091, "USER",
+								"TASK_012");
+					} catch (Exception e1) {
+						System.out.println("User Start Fail !!!");
+					}
+					System.out.println(back);
+				}
+			}
+		});
+
+		b_user_check.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				for (int i = 1; i <= 3; i++) {
+					String back = null;
+					try {
+						back = MonitorSetting.Usually(MonitorSetting.local_server_info.get(i), 8091, "USER",
+								"TASK_013");
+						double val = Double.parseDouble(JSONObject.fromObject(back).getString("PROGRESS"));
+						val = val * 100;
+						String pri = "" + val;
+						pri = pri.substring(0, 3) + "%";
+						progress[i].setText(pri);
+
+					} catch (Exception e1) {
+						System.out.println("User Check Fail !!!");
+					}
+					System.out.println(back);
+				}
+			}
+		});
+
+		b_collectdata.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
+
+	} // end addAction
+
+	public static void main(String[] args) {
+		new MonitorWindow();
+	}
+
+}
